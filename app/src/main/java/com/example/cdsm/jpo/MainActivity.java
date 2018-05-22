3d7b32cde8485450a74268e5bebfcf81a3e69777
Sylvain CHICCAM <schiccam@hotmail.fr>
2018-05-22 08:43:26 +0200

    AJout communes

--- app/src/main/java/com/example/cdsm/jpo/MainActivity.java
+++ app/src/main/java/com/example/cdsm/jpo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cdsm.jpo;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.constraint.ConstraintLayout;
@@ -10,10 +12,15 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import junit.framework.Test;
+
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout layout;
     SQLiteDatabase db;
+    Boolean Testdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Création de la base de données à l'ouverture de l'application
-        db = this.openOrCreateDatabase("JPO", MODE_PRIVATE, null);
+        db = this.openOrCreateDatabase("db_JPO", MODE_PRIVATE, null);
+
+        //Création de la table  Formation
         db.beginTransaction();
         try {
             //Table Formation
@@ -45,11 +54,20 @@ public class MainActivity extends AppCompatActivity {
                             + "  FormPdf TEXT);";
             db.execSQL(CreateTable);
             db.setTransactionSuccessful();
-            Log.e("Table 1","ok");
 
+
+        } catch (SQLException e) {
+            System.out.print(e);
+        } finally {
+            db.endTransaction();
+        }
+
+        //Création de la table Fiche
+        db.beginTransaction();
+        try {
             //Table Fiche
-            //TODO Modifier la table en fonction des infos pour les check box
-            CreateTable =
+//TODO Modifier la table en fonction des infos pour les check box (Emploi , Déscolarisé , Autre)
+            String CreateTable =
                     "CREATE TABLE IF NOT EXISTS Fiche("
                             + "  FId INTEGER PRIMARY KEY AUTOINCREMENT,"
                             + "  FNom TEXT,"
@@ -57,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                             + "  FSexe TEXT,"
                             + "  FDateNaiss TEXT,"
                             + "  FLieuNaiss TEXT,"
-                            + "  FNationalité TEXT,"
+                            + "  FNationalite TEXT,"
                             + "  FAdresse TEXT,"
                             + "  FCP INTEGER,"
                             + "  FVille TEXT,"
@@ -72,14 +90,54 @@ public class MainActivity extends AppCompatActivity {
                             + "  FOrigineDemande TEXT);";
             db.execSQL(CreateTable);
             db.setTransactionSuccessful();
-            Log.e("Table 2","ok");
-
         } catch (SQLException e) {
             System.out.print(e);
         } finally {
             db.endTransaction();
         }
 
+        //Importation des communes
+
+        // Test si la table Commune existe
+        try
+        {
+            long count = getProfilesCount();
+            Testdb = true;
+        }
+        catch (Exception e)
+        {
+            Log.e("Erreur :",""+e);
+            Testdb = false;
+        }
+
+        // Création ou non de la table en fonction du test
+        if(!Testdb)
+        {
+            Log.i("Communes","Début importation");
+            db = this.openOrCreateDatabase("db_Commune", MODE_PRIVATE, null);
+            Scanner scan = new Scanner (getResources().openRawResource(R.raw.commune));
+            String query = "";
+            while(scan.hasNextLine())
+            {
+                query += scan.nextLine() + "\n";
+                if(query.trim().endsWith(";"))
+                {
+                    db.execSQL(query);
+                    query = "";
+                }
+            }
+            Log.i("Communes","Fin importation");
+        }
+
     }
+
+    public long getProfilesCount() {
+        SQLiteDatabase database = this.openOrCreateDatabase("db_Commune", MODE_PRIVATE, null);
+        long count = DatabaseUtils.queryNumEntries(database, "commune");
+        database.close();
+        return count;
+    }
+
+
 
 }
